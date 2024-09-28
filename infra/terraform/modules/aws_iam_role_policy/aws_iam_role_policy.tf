81c0b66d138d181f187b266912f87610fd449a59
Jeevan N <144995385+Jeevanreddy-2005@users.noreply.github.com>
2024-09-28 15:43:21 +0530

    Revert "Automated PR"

--- infra/terraform/modules/aws_iam_role_policy/aws_iam_role_policy.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_iam_role_policy" "this" {
-  name   = var.name
-  role   = var.role
-  policy = var.policy
-}
--- infra/terraform/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "region" {
-  description = "AWS region in which the project needs to be setup (us-east-1, ca-west-1, eu-west-3, etc)"
-}
